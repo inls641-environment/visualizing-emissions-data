5246e65f612dc2d4663fcbefcda0e5c312d5a242
eranatza <99348524+eranatza@users.noreply.github.com>
2022-04-20 15:22:28 -0400

    Elizabeth edited 4/20

---
+++ 042022_final641/plot_map.js
@@ -0,0 +1,137 @@
+//  let us_map_data = async () => {
+//      return await d3.json("us-states.json").then(function(data) {
+//          console.log(data)
+//          return data[0]
+//      })
+//  }
+//
+// console.log(us_map_data)
+
+class USA_MAP {
+    constructor(container_id, emissions) {
+        this.container_id = container_id;
+        this.emissions = emissions;
+        this.svg = d3.select('#'+this.container_id)
+
+    };
+
+    async render(_subset) {
+
+        // Index the state data by state name for easy lookup.
+        let us_map_data = await d3.json('us-states.json', d => d)
+        //console.log(us_map_data)
+
+        // Create a projection.
+        let projection = d3.geoAlbersUsa()
+            .translate([1000 / 2, 600 / 2]) // translate to center of the SVG element.
+            .scale([1000]); // scale the projection so see the entire US
+
+        // Define the path generator using the projection.
+        let path = d3.geoPath().projection(projection);
+
+        // Select the SVG element for the map.
+        let svg = d3.select("#us_map");
+
+        let cities = Array.from(d3.group(emissions, d => d.AccountName));
+
+        if (_subset == 'Coal') {
+            cities = cities.filter(d => d[1][0]['Coal'] > d3.mean(emissions, d => d.Coal))
+        }
+        if (_subset == 'Gas') {
+            cities = cities.filter(d => d[1][0]['Gas'] > d3.mean(emissions, d => d.Gas))
+        }
+        if (_subset == 'Oil') {
+            cities = cities.filter(d => d[1][0]['Oil'] > d3.mean(emissions, d => d.Oil))
+        }
+        if (_subset == 'Nuclear') {
+            cities = cities.filter(d => d[1][0]['Nuclear'] > d3.mean(emissions, d => d.Nuclear))
+        }
+        if (_subset == 'Hydro') {
+            cities = cities.filter(d => d[1][0]['Hydro'] > d3.mean(emissions, d => d.Hydro))
+        }
+        if (_subset == 'Bioenergy') {
+            cities = cities.filter(d => d[1][0]['Bioenergy'] > d3.mean(emissions, d => d.Bioenergy))
+        }
+        if (_subset == 'Wind') {
+            cities = cities.filter(d => d[1][0]['Wind'] > d3.mean(emissions, d => d.Wind))
+        }
+        if (_subset == 'Geothermal') {
+            cities = cities.filter(d => d[1][0]['Geothemal'] > d3.mean(emissions, d => d.Geothemal))
+        }
+        if (_subset == 'Solar') {
+            cities = cities.filter(d => d[1][0]['Solar'] > d3.mean(emissions, d => d.Solar))
+        }
+        if (_subset == 'Other') {
+            cities = cities.filter(d => d[1][0]['Other'] > d3.mean(emissions, d => d.Other))
+        }
+
+        if (_subset == 'BASIC') {
+            cities = cities.filter(d => d[1][0]['Total (Traditional)'] == 0)
+        }
+
+        if (_subset == 'Traditional') {
+            cities = cities.filter(d => d[1][0]['Total(BASIC)'] === 0)
+        }
+
+        if (_subset == 'Renewable') {
+            cities = cities.filter(d => d[1][0]['Reneweable'] > d[1][0]['Non-Renewable'])
+        }
+
+        if (_subset == 'Non-Renewable') {
+            cities = cities.filter(d => d[1][0]['Non-Renewable'] > d[1][0]['Reneweable'])
+        }
+
+        // Define a color scale for the map.
+        //let colormap = d3.scaleLinear().domain([10,30]).range(["silver", "red"]);
+
+        // draw state lines
+        this.svg.append("g")
+            .attr("class", "states")
+            .selectAll("path")
+            .data(us_map_data.features)
+            .enter().append("path")
+            .attr("fill", 'peachpuff')
+            .attr("d", path)
+            .attr("stroke", "grey")
+
+
+
+        this.svg.selectAll(".city").data(cities, d=> d).join(
+
+            enter => enter.append('circle')
+                .attr("class", "city")
+                .attr("cx", d => {
+                    return projection([d[1][0]['long'], d[1][0]['lat']])[0]
+                })
+                .attr("cy", d => {
+                    return projection([d[1][0]['long'], d[1][0]['lat']])[1]
+                })
+                .attr("r", 4)
+                .attr("fill", "white")
+                .attr("stroke", "black")
+                .on("click", (event,d) => {
+                    document.getElementById("cityIDfromMap").innerHTML = d[1][0]['ID']; //write to HYML trick from: https://sebhastian.com/display-javascript-variable-html/
+                    //var cities_plot = new city_scatterplot("scatter_plot", emissions)
+                    console.log(d[1][0]['ID'])
+                    var city_pie = new city_piechart("ummm_pie", emissions, d[1][0]['ID']);
+                })
+                //.on("mouseover", (event, d) => {
+                //    document.getElementById("details").innerHTML = "The " + d[1][0]['AccountName'] +
+                //        " gets " + Math.round(d[1][0]['Reneweable'] * 100) + "% of their energy from renewable sources.";
+                //})
+                //.on("mouseout", (event, d) => {
+                //    document.getElementById("details").innerHTML = ""})
+
+                .raise(),
+
+            update => update.raise(),
+
+            exit => exit.remove())
+
+        //this.svg.on("click", function(event,d) { console.log(d.cities); });
+    }
+};
+
+// let cities_map = new USA_MAP("us_map");
+// //let path_data = d3.json('us-states.json');
+// cities_map.render("us");
---
+++ 042022_final641/plot_piechart.js
@@ -0,0 +1,96 @@
+//copied and modified from https://d3-graph-gallery.com/graph/pie_annotation.html
+
+var pie_width = 350
+pie_height = 350
+pie_margin = 30
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+var radius = Math.min(pie_width, pie_height) / 2 - pie_margin
+
+class city_piechart{
+    constructor(container_id, emissions, selected_city) {
+        this.container_id = container_id;
+        this.emissions = emissions;
+        this.selected_city = selected_city;
+        this.svg = d3.select('#'+this.container_id)
+        this.render(selected_city)
+    }
+    render(selected_city) {
+        d3.selectAll("#" +this.container_id +"> *").remove(); //https://stackoverflow.com/questions/22452112/nvd3-clear-svg-before-loading-new-chart
+        //create new array of cities with average renewable and nonrenewable
+        let cities = Array.from(d3.group(emissions, d => d.ID));
+
+        var avgDataset = []
+        for (let cityIndex in cities) {
+            let cityId = cities[cityIndex][1][0]['ID'];
+            if (selected_city === cityId) {
+                let cityName = cities[cityIndex][1][0]['AccountName'];
+                let coal_avg = d3.mean(cities[cityIndex][1], d => d.Coal);
+                let gas_avg = d3.mean(cities[cityIndex][1], d => d.Gas);
+                let oil_avg = d3.mean(cities[cityIndex][1], d => d.Oil);
+                let nuclear_avg = d3.mean(cities[cityIndex][1], d => d.Nuclear);
+                let hydro_avg = d3.mean(cities[cityIndex][1], d => d.Hydro);
+                let bioenergy_avg = d3.mean(cities[cityIndex][1], d => d.Bioenergy);
+                let wind_avg = d3.mean(cities[cityIndex][1], d => d.Wind);
+                let geothermal_avg = d3.mean(cities[cityIndex][1], d => d.Geothermal);
+                let solar_avg = d3.mean(cities[cityIndex][1], d => d.Solar);
+                let other_avg = d3.mean(cities[cityIndex][1], d => d.Other);
+
+                avgDataset.push({'ID': cityId, 'city': cityName, "Coal": coal_avg, "Gas": gas_avg, "Oil": oil_avg,
+                    "Nuclear": nuclear_avg, "Hydro": hydro_avg, "Bioenergy": bioenergy_avg, "Wind": wind_avg,
+                    "Geothemal": geothermal_avg, "Solar": solar_avg, "Other": other_avg
+                })
+            }
+        };
+
+        const g = this.svg.append("g")
+            .attr("width", pie_width)
+            .attr("height", pie_height)
+            .append("g")
+            .attr("transform", "translate(" + pie_width / 2 + "," + pie_height / 2 + ")");
+
+        // Create dummy data
+        var testCity = avgDataset[0];
+        const individual_data = {"Coal": testCity['Coal'], "Gas":testCity['Gas'], "Oil": testCity['Oil'],
+            "Nuclear": testCity['Nuclear'], "Hydro": testCity['Hydro'], "Bioenergy": testCity['Bioenergy'], "Wind": testCity['Wind'],
+            "Geothemal": testCity['Geothermal'], "Solar": testCity['Solar'], "Other": testCity['Other']};
+        //console.log(individual_data);
+
+        //remove sources with 0 or undefined values
+        for (let resource in individual_data) {
+            if (!Number(individual_data[resource]) || individual_data[resource] === 0){
+                delete individual_data[resource];
+            };
+        };
+
+
+        //console.log(individual_data);
+
+        // set the color scale
+        const color = d3.scaleOrdinal().range(d3.schemeSet2);
+
+        // Compute the position of each group on the pie:
+        const pie = d3.pie().value(function(d) {return d[1]})
+        const data_ready = pie(Object.entries(individual_data))
+
+        // shape helper to build arcs:
+        const arcGenerator = d3.arc()
+            .innerRadius(0)
+            .outerRadius(radius)
+
+        // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+        g.selectAll('mySlices').data(data_ready).join('path')
+            .attr('d', arcGenerator)
+            .attr('fill', function(d){ return(color(d.data[0])) })
+            .attr("stroke", "black")
+            .style("stroke-width", "2px")
+            .style("opacity", 0.7)
+
+        // Now add the annotation. Use the centroid method to get the best coordinates
+        g.selectAll('mySlices').data(data_ready).join('text')
+            .text(function(d){ return d.data[0]})
+            .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
+            .style("text-anchor", "middle")
+            .style("font-size", 17)
+    }
+}
